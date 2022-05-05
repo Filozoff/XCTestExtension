1a150b6ec07fb3337c64dfc29292b882d6cbf1ff
Kamil Wyszomierski <kamil.wyszomierski@gmail.com>
2022-05-05 22:08:45 +0200

    Setup package

---
+++ Package.swift
@@ -0,0 +1,25 @@
+// swift-tools-version:5.6
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "XCTestExtension",
+    products: [
+        .library(
+            name: "XCTestExtension",
+            targets: ["XCTestExtension"]
+        )
+    ],
+    dependencies: [],
+    targets: [
+        .target(
+            name: "XCTestExtension",
+            dependencies: []
+        ),
+        .testTarget(
+            name: "XCTestExtensionTests",
+            dependencies: ["XCTestExtension"]
+        ),
+    ]
+)
