a74515ca6fa82c2e626680daac193caa9f02d402
Kamil Wyszomierski <kamil.wyszomierski@gmail.com>
2022-10-23 15:28:00 +0200

    Add `lowercase` helper method

--- Scripts/common.sh
+++ Scripts/common.sh
@@ -4,6 +4,10 @@ set -Eeuo pipefail
 
 # FUNCTIONS
 
+function lowercase {
+    echo "$1" | awk '{print tolower($0)}'
+}
+
 function swift_package_name() {
     swift package describe | awk '/Name:/ { print $2; exit; }'
 }
