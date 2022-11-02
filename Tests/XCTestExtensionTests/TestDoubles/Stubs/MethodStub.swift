7dd283b75f8fd715a598766e0d87b54229614638
Kamil Wyszomierski <kamil.wyszomierski@gmail.com>
2022-11-02 23:02:47 +0100

    Add stub async methods

--- Tests/XCTestExtensionTests/TestDoubles/Stubs/MethodStub.swift
+++ Tests/XCTestExtensionTests/TestDoubles/Stubs/MethodStub.swift
@@ -1,3 +1,5 @@
+import Foundation
+
 struct MethodStub {
 
     static func failable<E>(with error: E) throws where E: Error {
@@ -5,4 +7,27 @@ struct MethodStub {
     }
 
     static func nonFailable() throws { }
+
+    @available(iOS 13.0, macOS 10.15, tvOS 13.0, watchOS 6.0, *)
+    static func asynchronous(after timeInterval: TimeInterval) async {
+        await asynchronous(return: (), after: timeInterval)
+    }
+
+    @available(iOS 13.0, macOS 10.15, tvOS 13.0, watchOS 6.0, *)
+    static func asynchronous<T>(return value: T, after timeInterval: TimeInterval) async -> T {
+        try? await Task.sleep(seconds: timeInterval)
+        return value
+    }
+
+    @available(iOS 13.0, macOS 10.15, tvOS 13.0, watchOS 6.0, *)
+    static func asynchronousThrowable<T>(return value: T, after timeInterval: TimeInterval) async throws -> T {
+        try await Task.sleep(seconds: timeInterval)
+        return value
+    }
+
+    @available(iOS 13.0, macOS 10.15, tvOS 13.0, watchOS 6.0, *)
+    static func asynchronousThrowing(error: Error, after timeInterval: TimeInterval) async throws {
+        try await Task.sleep(seconds: timeInterval)
+        throw error
+    }
 }
