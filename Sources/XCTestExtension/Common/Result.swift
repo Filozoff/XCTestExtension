63f401e90d5ce72d2c2c6097250913bcf499b6be
Kamil Wyszomierski <kamil.wyszomierski@gmail.com>
2022-11-06 20:37:41 +0100

    Make `Result` internal and specify it's avalaibility

--- Sources/XCTestExtension/Common/Result.swift
+++ Sources/XCTestExtension/Common/Result.swift
@@ -1,8 +1,9 @@
 import Foundation
 
+@available(iOS 13.0, macOS 10.15, tvOS 13.0, watchOS 6.0, *)
 extension Result where Failure == Error, Success: Sendable {
 
-    public init(catching body: () async throws -> Success) async {
+    init(catching body: () async throws -> Success) async {
         do {
             let result = try await body()
             self = .success(result)
