ca23faffebe7003de1362d9532589c46c3975191
Kamil Wyszomierski <kamil.wyszomierski@gmail.com>
2023-03-21 22:37:41 +0100

    Tweak `XCTExpectFailure` descriptions

--- Tests/XCTestExtensionTests/AssertionsTests/XCTAssertThrowsErrorEqualTests.swift
+++ Tests/XCTestExtensionTests/AssertionsTests/XCTAssertThrowsErrorEqualTests.swift
@@ -8,17 +8,17 @@ final class XCTAssertThrowsErrorEqualTests: XCTestCase {
     }
 
     func test_givenFailableMethodWithDifferentError_whenAssertThrowsErrorEqual_thenTestFailure() throws {
-        XCTExpectFailure("Should fail because of not equal error")
+        XCTExpectFailure("Should fail due to not equal error")
         XCTAssertThrowsErrorEqual(try MethodStub.failable(with: EquatableError.errorOne), EquatableError.errorTwo)
     }
 
     func test_givenFailableMethodWithDifferentErrorType_whenAssertThrowsErrorEqual_thenTestFailure() throws {
-        XCTExpectFailure("Should fail because of error type mismatch")
+        XCTExpectFailure("Should fail due to error type mismatch")
         XCTAssertThrowsErrorEqual(try MethodStub.failable(with: SimpleEnumError.errorTwo), EquatableError.errorTwo)
     }
 
     func test_givenNonFailableMethod_whenAssertThrowsErrorEqual_thenTestFailure() throws {
-        XCTExpectFailure("Should fail because of not throwing method")
+        XCTExpectFailure("Should fail due to not throwing method")
         XCTAssertThrowsErrorEqual(try MethodStub.nonFailable(), EquatableError.errorTwo)
     }
 
@@ -31,19 +31,19 @@ final class XCTAssertThrowsErrorEqualTests: XCTestCase {
 
     @available(iOS 13.0, macOS 10.15, tvOS 13.0, watchOS 6.0, *)
     func test_givenFailableMethodWithDifferentError_whenAsyncAssertThrowsErrorEqual_thenTestFailure() async throws {
-        XCTExpectFailure("Should fail because of not equal error")
+        XCTExpectFailure("Should fail due to not equal error")
         await XCTAssertThrowsErrorEqual(try MethodStub.failable(with: EquatableError.errorOne), EquatableError.errorTwo)
     }
 
     @available(iOS 13.0, macOS 10.15, tvOS 13.0, watchOS 6.0, *)
     func test_givenFailableMethodWithDifferentErrorType_whenAsyncAssertThrowsErrorEqual_thenTestFailure() async throws {
-        XCTExpectFailure("Should fail because of error type mismatch")
+        XCTExpectFailure("Should fail due to error type mismatch")
         await XCTAssertThrowsErrorEqual(try MethodStub.failable(with: SimpleEnumError.errorTwo), EquatableError.errorTwo)
     }
 
     @available(iOS 13.0, macOS 10.15, tvOS 13.0, watchOS 6.0, *)
     func test_givenNonFailableMethod_whenAsyncAssertThrowsErrorEqual_thenTestFailure() async throws {
-        XCTExpectFailure("Should fail because of not throwing method")
+        XCTExpectFailure("Should fail due to not throwing method")
         await XCTAssertThrowsErrorEqual(try MethodStub.nonFailable(), EquatableError.errorTwo)
     }
 }
