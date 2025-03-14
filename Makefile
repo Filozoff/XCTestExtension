f94caf742ecb3e130e43bf83b5c4fec898671786
Kamil Wyszomierski <kamil.wyszomierski@gmail.com>
2025-03-14 14:14:15 +0100

    Modify `code_coverage` script. Update Makefile

--- Makefile
+++ Makefile
@@ -3,12 +3,21 @@ derived_data_dir := $(build_dir)/derived_data
 destination_device := platform=iOS Simulator,name=iPhone 16
 scheme := XCTestExtension
 scripts_dir := ./Scripts
+test_coverage_report_dir := $(build_dir)/coverage_reports
 test_output_dir := $(build_dir)/test_output
+test_targets := XCTestExtensionTests
 
 test:
 	echo "Testing..."
 	sh "$(scripts_dir)/run_tests.sh" \
-		--derived-data-path "$($derived_data_dir)" \
+		--derived-data-path "$(derived_data_dir)" \
 		--device "$(destination_device)" \
 		--output "$(test_output_dir)" \
 		--scheme "$(scheme)"
+
+code-coverage:
+	echo "Gathering code coverage..."
+	sh "$(scripts_dir)/code_coverage.sh" \
+		--derived-data-path "$(derived_data_dir)" \
+		--output "$(test_coverage_report_dir)" \
+		--test-targets $(test_targets)
--- Scripts/code_coverage.sh
+++ Scripts/code_coverage.sh
@@ -4,37 +4,71 @@ set -Eeuo pipefail
 
 source "$(dirname "$0")"/common.sh
 
-# CONSTANTS
-
-readonly COVERAGE_DIR_NAME=".coverage"
-readonly SWIFT_BUILD_DIR_NAME=".build"
-readonly PACKAGE_NAME=$(swift_package_name)
-readonly PROFDATA_PATH="${SWIFT_BUILD_DIR_NAME}/debug/codecov/default.profdata"
-readonly BIN_PATH="${SWIFT_BUILD_DIR_NAME}/debug/${PACKAGE_NAME}PackageTests.xctest/Contents/MacOS/${PACKAGE_NAME}PackageTests"
-
 # FUNCTIONS
 
-function print_code_coverage() {
-    xcrun llvm-cov report \
-        "$BIN_PATH" \
-        -instr-profile="$PROFDATA_PATH" \
-        -ignore-filename-regex=".build|Tests" \
-        -use-color
-}
+function main() {
+    local profdata
+    local ignore_regex
+    local test_targets
 
-function export_code_coverage() {
-    local -r coverage_report_path="$COVERAGE_DIR_NAME/$PACKAGE_NAME.lcov"
+    IFS=',' read -r -a test_targets <<< "$TEST_TARGETS"
+    profdata=$(find "$DERIVED_DATA_PATH" -name "Coverage.profdata")
+    ignore_regex=".build|Tests"
 
-    mkdir -p "$COVERAGE_DIR_NAME"
+    mkdir -p "$OUTPUT_DIR"
 
-    xcrun llvm-cov export \
-        -format="lcov" "$BIN_PATH" \
-        -instr-profile="$PROFDATA_PATH" \
-        -ignore-filename-regex=".build|Tests" \
-        > "$coverage_report_path"
+    for test_target in "${test_targets[@]}"; do
+        local coverage_report_path
+        local binary_path
+        local xctest_path
+
+        coverage_report_path="$OUTPUT_DIR/$test_target.lcov"
+
+        printf '\n\nPreparing code coverage for %s...\n' "$test_target"
+
+        xctest_path=$(find "$DERIVED_DATA_PATH" -name "$test_target.xctest")
+        if [[ -z "$xctest_path" ]]; then
+            echo "Error: XCTest bundle not found for target '$test_target'"
+            exit 1
+        fi
+
+        binary_path="$xctest_path/$test_target"
+
+        # Print coverage
+        xcrun llvm-cov report "$binary_path" \
+            -ignore-filename-regex="$ignore_regex" \
+            -instr-profile="$profdata" \
+            -use-color
+
+        # Export coverage
+        xcrun llvm-cov show "$binary_path" \
+            -ignore-filename-regex="$ignore_regex" \
+            -instr-profile="$profdata" \
+            > "$coverage_report_path"
+    done
 }
 
 # ENTRY POINT
 
-print_code_coverage
-export_code_coverage
+while [[ $# -gt 0 ]]; do
+    case $1 in
+    -p| --derived-data-path)
+        DERIVED_DATA_PATH=${2}
+        shift 2
+    ;;
+    -o| --output)
+        OUTPUT_DIR=${2}
+        shift 2
+    ;;
+    -t| --test-targets)
+        TEST_TARGETS=${2}
+        shift 2
+    ;;
+    *)
+        echo "Unknown argument: '${1}'"
+        exit 1
+    ;;
+    esac
+done
+
+main
